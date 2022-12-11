98dc2900fbf90806cac265336e6ae1f9cfce71d2
1ntelloRW <61648396+1ntelloRW@users.noreply.github.com>
2022-12-11 18:44:58 +0100

    Cleanup C1

--- AdventOfCode/AdventOfCode.Challenges/Challenge1.cs
+++ AdventOfCode/AdventOfCode.Challenges/Challenge1.cs
@@ -8,7 +8,6 @@ namespace AdventOfCode.Challenges
 {
     public class Challenge1
     {
-      
         public void Run()
         {
             string[] calories = File.ReadAllLines("Inputs/challenge1.txt");
@@ -38,29 +37,5 @@ namespace AdventOfCode.Challenges
             var amountOfCalories = hardestWorkingElves.Sum(x => x.Value);
             Console.WriteLine($"The three hardest working elves are carrying { amountOfCalories } calories");
         }
-
-        // Don't use this its mean
-        public void RunDifferent(string path)
-        {
-            string[] calories = File.ReadAllLines(path);
-            int[] result = new int[calories.Length];
-            int current, counter;
-            current = 0;  counter = 1;
-
-            foreach (var c in calories)
-                if (c != string.Empty)
-                    current += Int32.Parse(c);
-                else
-                {
-                    result[counter - 1] = current;
-                    current = 0;
-                    counter++;
-                }
-
-            Console.WriteLine($"Elf carrying the most calories is elf number { Array.IndexOf(result, result.Max(), 0, result.Length) + 1 } carrying { result.Max() }");
-        }
-
-       
-
     }
 }
