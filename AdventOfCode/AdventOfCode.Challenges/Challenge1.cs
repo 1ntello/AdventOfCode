aaeb2bb3eec28a53580deb42e05c97c3f735b39b
1ntelloRW <61648396+1ntelloRW@users.noreply.github.com>
2022-12-11 12:27:43 +0100

    Challenge 1
    I am lazy

---
+++ AdventOfCode/AdventOfCode.Challenges/Challenge1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO; 
+
+
+namespace AdventOfCode.Challenges
+{
+    public class Challenge1
+    {
+       
+        public Challenge1()
+        {
+
+        }
+
+        public void Run()
+        {
+            string[] calories = File.ReadAllLines("Inputs/challenge1.txt");
+            Dictionary<int, int> ElvesAndCalories = new Dictionary<int, int>();
+
+            int elf = 1;
+            int current = 0;
+
+            foreach (var c in calories)
+            {
+                if (c != String.Empty)
+                    current += Int32.Parse(c);
+                else
+                {
+                    ElvesAndCalories.Add(elf, current);
+                    elf++;
+                    current = 0; 
+                }
+            }
+
+            var fatso = ElvesAndCalories.OrderBy(x => x.Value).Last();
+            Console.WriteLine($"Elf carrying the most calories is elf number { fatso.Key } carrying { fatso.Value }");
+
+        }
+
+
+    }
+}
---
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using AdventOfCode.Challenges;
+
+namespace AdventOfCode.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Challenge1().Run();
+        }
+    }
+}
